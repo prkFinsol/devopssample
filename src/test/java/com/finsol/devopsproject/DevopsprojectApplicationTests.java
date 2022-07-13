379c6205b734e0916ccf447c5f803f70c6b79643
prk <ramakrishna.p@finsolconsultancy.com>
2022-07-13 12:04:57 +0530

    fist commit ;

---
+++ src/test/java/com/finsol/devopsproject/DevopsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finsol.devopsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
